edaa223b03da461426a18a62df939957ef755898
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-06-08 12:27:05 +0700

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './modules/app-layout/app-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayoutComponent,
+        // children: [
+        //     { path: '', redirectTo: 'home', pathMatch: 'full' },
+        //     {
+        //         path: 'home',
+        //         loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+        //     },
+        //     {
+        //         path: 'course',
+        //         loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule)
+        //     },
+        //     {
+        //         path: 'calendar',
+        //         loadChildren: () => import('./modules/calendar/calendar.module').then(m => m.CalendarModule)
+        //     },
+        //     {
+        //         path: 'chat',
+        //         loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
+        //     },
+        //     {
+        //         path: 'forum',
+        //         loadChildren: () => import('./modules/forum/forum.module').then(m => m.ForumModule)
+        //     },
+        //     {
+        //         path: 'playground',
+        //         loadChildren: () => import('./modules/playground/playground.module').then(m => m.PlaygroundModule)
+        //     },
+        //     {
+        //         path: 'profile',
+        //         loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
+        //     },
+        //     {
+        //         path: 'streaming',
+        //         loadChildren: () => import('./modules/streaming/streaming.module').then(m => m.StreamingModule)
+        //     },
+        //     {
+        //         path: 'auth',
+        //         loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+        //     },
+        // ]
+    },
+];
