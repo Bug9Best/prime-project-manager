c4058f52f8976fc78a5c125483697b1d50ba444d
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-06-10 05:14:13 +0700

    done not found component

--- src/app/shared/components/notfound/notfound.component.ts
+++ src/app/shared/components/notfound/notfound.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    RouterModule
+  ],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.scss'
 })
