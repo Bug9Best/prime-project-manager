4158c18ae4d27404d8a6e3892855161180e22218
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-06-09 18:44:50 +0700

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,48 +1,24 @@
 import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './modules/app-layout/app-layout.component';
+import { NotfoundComponent } from './shared/components/notfound/notfound.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AppLayoutComponent,
-        // children: [
-        //     { path: '', redirectTo: 'home', pathMatch: 'full' },
-        //     {
-        //         path: 'home',
-        //         loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-        //     },
-        //     {
-        //         path: 'course',
-        //         loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule)
-        //     },
-        //     {
-        //         path: 'calendar',
-        //         loadChildren: () => import('./modules/calendar/calendar.module').then(m => m.CalendarModule)
-        //     },
-        //     {
-        //         path: 'chat',
-        //         loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
-        //     },
-        //     {
-        //         path: 'forum',
-        //         loadChildren: () => import('./modules/forum/forum.module').then(m => m.ForumModule)
-        //     },
-        //     {
-        //         path: 'playground',
-        //         loadChildren: () => import('./modules/playground/playground.module').then(m => m.PlaygroundModule)
-        //     },
-        //     {
-        //         path: 'profile',
-        //         loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
-        //     },
-        //     {
-        //         path: 'streaming',
-        //         loadChildren: () => import('./modules/streaming/streaming.module').then(m => m.StreamingModule)
-        //     },
-        //     {
-        //         path: 'auth',
-        //         loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
-        //     },
-        // ]
+        children: [
+            { path: '', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'sprint', loadChildren: () => import('./modules/sprint/sprint.module').then(m => m.SprintModule) },
+            { path: 'backlog', loadChildren: () => import('./modules/backlog/backlog.module').then(m => m.BacklogModule) },
+            { path: 'timeline', loadChildren: () => import('./modules/timeline/timeline.module').then(m => m.TimelineModule) },
+            { path: 'board', loadChildren: () => import('./modules/board/board.module').then(m => m.BoardModule) },
+            { path: 'list', loadChildren: () => import('./modules/list/list.module').then(m => m.ListModule) },
+            { path: 'project-setting', loadChildren: () => import('./modules/project-setting/project-setting.module').then(m => m.ProjectSettingModule) },
+        ]
     },
+    { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+    { path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
+    { path: 'notfound', component: NotfoundComponent },
+    { path: '**', redirectTo: '/notfound' },
 ];
