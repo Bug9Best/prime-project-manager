1ad9e365365f09457bcdd4993925de423d577354
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-06-10 16:35:49 +0700

    create new component

---
+++ src/app/shared/components/assign-me/assign-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'assign-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './assign-me.component.html',
+  styleUrl: './assign-me.component.scss'
+})
+export class AssignMeComponent {
+
+}
