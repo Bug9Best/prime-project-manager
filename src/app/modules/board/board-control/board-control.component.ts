1002a8f605180b7925dd48c7e45b40d63c47a7a8
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-06-12 16:57:17 +0700

    doen board component

---
+++ src/app/modules/board/board-control/board-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'board-control',
+  templateUrl: './board-control.component.html',
+  styleUrl: './board-control.component.scss'
+})
+export class BoardControlComponent {
+
+}
--- src/app/modules/board/board.module.ts
+++ src/app/modules/board/board.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BoardComponent } from './board.component';
+import { PrimeNGModule } from '../../shared/primeng.module';
+import { BoardControlComponent } from './board-control/board-control.component';
 
 const routes: Routes = [
   {
@@ -12,11 +14,14 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    BoardComponent
+    BoardComponent,
+    BoardControlComponent
   ],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    PrimeNGModule,
+
   ]
 })
 export class BoardModule { }
