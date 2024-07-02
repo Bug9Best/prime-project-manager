bb7249a53107fb819d83024f3ccdd26eeddd0102
Phuri Siripool <85283698+Phuriq@users.noreply.github.com>
2024-07-02 22:19:18 +0700

    UP_Project-Dashborad

--- src/app/pages/project-dashboard/project-dashboard.component.ts
+++ src/app/pages/project-dashboard/project-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-dashboard',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './project-dashboard.component.scss'
 })
 export class ProjectDashboardComponent {
+  template: any = -1;
 
+  constructor(
+    private router: Router,
+  ) { }
+
+  onCancel() {
+    this.router.navigate(['/home']);
+  }
+
+  onSelectProject() {
+    this.router.navigate(['/dashboard']);
+  }
+  onCreateMoreProject() {
+    this.router.navigate(['/project-create']);
+  }
 
 
 }
