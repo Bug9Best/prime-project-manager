2d85889322e7f501b216feaf2197ee2ddafee4b6
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-07 19:21:47 +0700

    update

--- src/app/shared/components/total-project/total-project.component.ts
+++ src/app/shared/components/total-project/total-project.component.ts
@@ -1,50 +1,51 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ChartModule } from 'primeng/chart';
-import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'total-project',
   standalone: true,
-  imports: [ButtonModule, TableModule, ChartModule],
+  imports: [ButtonModule, ChartModule],
   templateUrl: './total-project.component.html',
   styleUrl: './total-project.component.scss',
 })
 export class TotalProjectComponent {
   data: any;
-
   options: any;
 
   ngOnInit() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
+    setTimeout(() => {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
 
-    this.data = {
-      datasets: [
-        {
-          data: [70, 30],
-          backgroundColor: [
-            documentStyle.getPropertyValue('--red-400'),
-            documentStyle.getPropertyValue('--blue-500'),
-          ],
-          hoverBackgroundColor: [
-            documentStyle.getPropertyValue('--red-300'),
-            documentStyle.getPropertyValue('--blue-400'),
-          ],
-        },
-      ],
-      labels: ['Bug', 'Issue'],
+      this.data = {
+        datasets: [
+          {
+            data: [70, 30],
+            backgroundColor: [
+              documentStyle.getPropertyValue('--red-400'),
+              documentStyle.getPropertyValue('--blue-500'),
+            ],
+            hoverBackgroundColor: [
+              documentStyle.getPropertyValue('--red-300'),
+              documentStyle.getPropertyValue('--blue-400'),
+            ],
+          },
+        ],
+        labels: ['Bug', 'Issue'],
 
-    };
-    this.options = {
-      cutout: '60%',
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor,
+      };
+      this.options = {
+        cutout: '60%',
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: {
+              color: textColor,
+            },
           },
         },
-      },
-    };
+      };
+    }, 100);
   }
 }
