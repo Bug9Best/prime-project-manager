c73904d8727af7d974a6aa98e7dd148c2962bd64
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-07 19:04:42 +0700

    update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   HostBinding,
   OnInit,
+  signal,
   ViewChild,
 } from '@angular/core';
 import {
@@ -25,7 +26,7 @@ import {
 import { finalize, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { addDays, getUnixTime } from 'date-fns';
-import { enUS } from 'date-fns/locale';
+import { enUS, is } from 'date-fns/locale';
 import {
   GridsterItem,
   GridType,
@@ -43,8 +44,64 @@ import { TotalProjectComponent } from '../../shared/components/total-project/tot
   providers: [GanttPrintService],
 })
 export class DashboardComponent implements OnInit {
+  isEdit = false;
   item: any[] = [];
-  options: any;
+
+  options: any = {
+    gridType: GridType.Fit,
+    compactType: CompactType.None,
+    margin: 10,
+    outerMargin: true,
+    outerMarginTop: null,
+    outerMarginRight: null,
+    outerMarginBottom: null,
+    outerMarginLeft: null,
+    useTransformPositioning: true,
+    mobileBreakpoint: 640,
+    useBodyForBreakpoint: false,
+    minCols: 1,
+    maxCols: 100,
+    minRows: 1,
+    maxRows: 100,
+    maxItemCols: 100,
+    minItemCols: 1,
+    maxItemRows: 100,
+    minItemRows: 1,
+    maxItemArea: 2500,
+    minItemArea: 1,
+    defaultItemCols: 1,
+    defaultItemRows: 1,
+    fixedColWidth: 105,
+    fixedRowHeight: 105,
+    keepFixedHeightInMobile: false,
+    keepFixedWidthInMobile: false,
+    scrollSensitivity: 10,
+    scrollSpeed: 20,
+    enableEmptyCellClick: false,
+    enableEmptyCellContextMenu: false,
+    enableEmptyCellDrop: false,
+    enableEmptyCellDrag: false,
+    enableOccupiedCellDrop: false,
+    emptyCellDragMaxCols: 50,
+    emptyCellDragMaxRows: 50,
+    ignoreMarginInRow: false,
+    draggable: {
+      enabled: false,
+    },
+    resizable: {
+      enabled: false,
+    },
+    swap: false,
+    pushItems: true,
+    disablePushOnDrag: false,
+    disablePushOnResize: false,
+    pushDirections: { north: true, east: true, south: true, west: true },
+    pushResizeItems: false,
+    displayGrid: DisplayGrid.OnDragAndResize,
+    disableWindowResize: false,
+    disableWarnings: false,
+    scrollToNewItems: false,
+  };
   dashboard: Array<GridsterItem> = [];
 
   static itemChange(item: any, itemComponent: any) {
@@ -56,7 +113,32 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.options = {
+    this.dashboard = [
+      { cols: 2, rows: 1, y: 0, x: 0, component: TotalIssueComponent },
+      { cols: 2, rows: 1, y: 0, x: 2, component: TotalProjectComponent },
+      { cols: 4, rows: 1, y: 1, x: 0, component: AssignMeComponent },
+    ];
+    console.log(this.options);
+  }
+
+  changedOptions(): void {
+    if (this.options.api && this.options.api.optionsChanged) {
+      this.options.api.optionsChanged();
+    }
+  }
+
+  removeItem(item: any) {
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem() {
+    this.dashboard.push();
+  }
+
+  toggleEdit() {
+    this.isEdit = !this.isEdit;
+    this.options =
+    {
       gridType: GridType.Fit,
       compactType: CompactType.None,
       margin: 10,
@@ -95,10 +177,10 @@ export class DashboardComponent implements OnInit {
       emptyCellDragMaxRows: 50,
       ignoreMarginInRow: false,
       draggable: {
-        enabled: true,
+        enabled: this.isEdit,
       },
       resizable: {
-        enabled: true,
+        enabled: this.isEdit,
       },
       swap: false,
       pushItems: true,
@@ -111,25 +193,5 @@ export class DashboardComponent implements OnInit {
       disableWarnings: false,
       scrollToNewItems: false,
     };
-
-    this.dashboard = [
-      { cols: 2, rows: 1, y: 0, x: 0, component: TotalIssueComponent },
-      { cols: 2, rows: 1, y: 0, x: 2, component: TotalProjectComponent},
-      { cols: 4, rows: 1, y: 1, x: 0, component: AssignMeComponent},
-    ];
-  }
-
-  changedOptions(): void {
-    if (this.options.api && this.options.api.optionsChanged) {
-      this.options.api.optionsChanged();
-    }
-  }
-
-  removeItem(item: any) {
-    this.dashboard.splice(this.dashboard.indexOf(item), 1);
-  }
-
-  addItem() {
-    this.dashboard.push();
   }
 }
